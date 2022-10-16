cc339e2736862556286a890431233c85416d6afb
FiveArcher76571 <vemuri.saicharan@gmail.com>
2022-10-16 16:58:24 -0400

    Buttonssssss!

---
+++ Assets/Scripts/SceneShifter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneShifter : MonoBehaviour
+{
+    public void to_tutorial() {
+        SceneManager.LoadScene("Tutorial and Story", LoadSceneMode.Single);
+    }
+    public void to_menu() {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+    public void to_gameover() {
+        SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
+    }
+    public void to_game() {
+        SceneManager.LoadScene("BlankScene", LoadSceneMode.Single);
+    }
+    public void to_exit() {
+        Application.Quit();
+    }
+}
