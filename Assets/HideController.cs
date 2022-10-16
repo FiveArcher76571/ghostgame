adf38b56e9bb3be778d12d4a4d1232f584ed952d
ethantho <ethantho@umich.edu>
2022-10-15 22:09:13 -0400

    Started HidingPlace

---
+++ Assets/HideController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+}
